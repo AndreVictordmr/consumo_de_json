405be8812c4f6c1c76db436fe5c3f86cf67100be
AndreVictorDmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-10-27 13:15:31 -0300

    alteraçao

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
 <?php
-    $url = "http://localhost/consumo_de_json/api/salgado_api.php?name=corndog&info=origem";
-    $resposta = file_get_contents($url);
-    $dado = json_decode($resposta,true);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Salgados congelados</title>
+        <title>Lista de pizza</title>
     </head>
     <body>
-        <pre>
-        <?=var_dump($dado);?>
-        </pre>
+
     </body>
 </html>
